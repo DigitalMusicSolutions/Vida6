60df30d6ecfbd5ab08c77a131eebcd94fe0dd735
Andrew Horwitz <ahwitz@gmail.com>
2018-05-29 13:03:35 -0400

    WIP: Webpack running and functional
    - Removed JSPM config
    - Moved Index.js import to index.html JS include
    
    - Still a lot of unneeded code

--- build-tools/gulpfile.js
+++ build-tools/gulpfile.js
@@ -1,25 +1,78 @@
+var path = require('path');
+
 var static_location = "static";
 var gulp = require('gulp');
 var server = require('gulp-express'); // Server tool, starts app.js
 var eslint = require('gulp-eslint'); // Linting
-var Builder = require('systemjs-builder'); // Build tool
 var sass = require('gulp-sass'); // SASS compiler
 var autoprefixer = require('gulp-autoprefixer'); // Pretty straightforward
 
-// Builds vida.js to vida.min.js, which is in turn aliased to vida5.min.js in root dir
-var buildJS = function()
+var webpack = require('webpack');
+
+let compiler;
+function makeWebpackConfig(production)
 {
-    var builder = new Builder(static_location, static_location + '/config.js');
-    return builder.buildStatic(
-        './' + static_location + '/js/vida.js', 
-        './' + static_location + '/js/vida.min.js', 
-        {minify: true}
-    ).then(function() {
-      console.log('Build complete at', new Date(), '.');
-    }).catch(function(err) {
-      console.log('Build error:');
-      console.log(err);
-    });
+    var config = {
+        mode: 'development',
+        entry: '../vida.js',
+        output: {
+            path: path.resolve(__dirname, 'static/js/'),
+            filename: 'vida.min.js',
+            library: 'vida',
+            libraryTarget: 'var'
+        },
+        resolve: {
+            extensions: ['.js']
+        }
+    };
+
+    if (production)
+    {
+        config.mode = 'production'; // Turn on a bunch of code optimizations/minification stuff
+        config.watch = false; // Prevent live changes on production
+
+        config.performance = { // This just avoids Webpack warning us about what we already know (that a ~600KB JS file is large)
+            maxAssetSize: 700000,
+            maxEntrypointSize: 700000
+        };
+
+        config.module = { // Babel transpilation for certain browser targets
+            rules: [
+                {
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [['env', {
+                                targets: {
+                                    browsers: browserSupport
+                                },
+                                useBuiltIns: true
+                            }]]
+                        }
+                    }
+                }
+            ]
+        };
+    }
+    else
+    {
+        config.mode = 'development'; // https://www.youtube.com/watch?v=oxEcGmD8WvA
+    }
+
+    return config;
+};
+
+function webpackDiag(err, stats)
+{
+    if (err)
+        return console.error('Build error:', err);
+
+    if (stats.compilation.errors && (stats.compilation.errors.length > 0))
+        return console.error('Compilation error:', stats.compilation.errors);
+
+    if (stats.compilation.warnings && (stats.compilation.warnings.length > 0))
+        return console.error('Compilation warnings:', stats.compilation.warnings);
 };
 
 // Builds and autoprefixes the SASS to CSS
@@ -66,6 +119,21 @@ gulp.task('build', function()
     buildJS();
 });
 
+gulp.task('js:develop', function (callback)
+{
+    if (!compiler)
+    {
+        console.log('Creating webpack compiler.');
+        compiler = webpack(makeWebpackConfig());
+    }
+
+    compiler.run(function (err, stats)
+    {
+        webpackDiag(err, stats);
+        callback();
+    });
+});
+
 gulp.task('develop', function()
 {
     gulp.watch(static_location + '/css/vida.scss', ['style:vida']);
@@ -73,10 +141,14 @@ gulp.task('develop', function()
     gulp.watch(static_location + '/js/**/*.js', ['lint']);
 
     gulp.watch([
-        static_location + '/js/**/*.js',
-        static_location + '/index.html',
-        static_location + '/css/*.css'
-    ], server.notify);
+        '/js/**/*.js'
+    ], ['js:develop']);
+
+    // gulp.watch([
+    //     static_location + '/js/**/*.js',
+    //     static_location + '/index.html',
+    //     static_location + '/css/*.css'
+    // ], server.notify);
 
     server.run(["app.js"]);
 });
